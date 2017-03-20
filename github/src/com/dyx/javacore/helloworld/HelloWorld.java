c54e0eb388ebfa728a28cd98082a9cd950d0f36b
dongyuxiang <2510330449@qq.com>
2017-03-20 17:43:32 +0800

    I hope success
    Signed-off-by: dongyuxiang <2510330449@qq.com>

---
+++ github/src/com/dyx/javacore/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dyx.javacore.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("你好世界");
+	}
+
+}
