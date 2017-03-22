2cedf4b3eb361a440c76596f8383ae921a31c186
dongyuxiang <1510095748@qq.com>
2017-03-22 23:04:37 +0800

    Signed-off-by: dongyuxiang <1510095748@qq.com>

---
+++ javacore/javacore/src/com/dyx/javacore/yunsuanfu/YunSuanFu.java
@@ -0,0 +1,54 @@
+package com.dyx.javacore.yunsuanfu;
+
+public class YunSuanFu {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+/*		int a=100;
+		short b=5;
+		System.out.println("a+b:"+a+b);
+		System.out.println("a+b:"+(a-b));
+		System.out.println("a+b:"+a*b);*/
+		//张三的成绩是否比李四高？
+/*		String name1="张三";
+		int score1=80;
+		String name2="李四";
+		int score2=90;
+		if(score1>score2)
+			System.out.println("张三的成绩比李四高");
+		else
+			System.out.println("李四的成绩比张三高");*/
+		//大象是否比乌龟更长寿
+/*		int daXiangAge=100,wuGuiAge=1000;
+		if(daXiangAge>wuGuiAge)
+			System.out.println("大象比乌龟长寿");
+		else
+			System.out.println("乌龟比大象长寿");*/
+		//判断两个boolean类型的变量a，b的结果
+/*		boolean a=true;
+		boolean b=false;
+		System.out.println(a && b);
+		System.out.println(a || b);
+		System.out.println(!b);
+		System.out.println((1<3)&&(1>2));
+		System.out.println((1<3)||(1>2));
+		System.out.println(!(1<3));*/
+		//三目运算
+	/*	int a=3,b=6;
+		String ab=a>b?"a":"b";
+		System.out.println(ab);*/
+	/*	String abc1="hello";
+		String abc2="world";
+		int code=5;
+		String abc3=abc1+abc2+code+"号";
+		System.out.println(abc3);*/
+		//强制转换运算符：
+/*		int b=100;
+		byte a=(byte)b;//将int型的b强制转换成byte型
+		System.out.println(a);*/
+		//递减递增运算符
+/*		int a=2,b=3;
+		 b=a+(++b);
+		 System.out.println(b);*/
+	}
+}
